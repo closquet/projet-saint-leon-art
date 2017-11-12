ae4d5597bec3950c8d1d0bc71f1a9edf9712af65
Eric Closquet <closquet.eric@live.be>
2017-11-12 02:24:10 +0100

    some issue corrected (bad loop, no places indexed)
    the post ok, same category of artist ok.
    todo : same places and same moment

--- app/wp-content/themes/mytheme/single-activites.php
+++ app/wp-content/themes/mytheme/single-activites.php
@@ -1,50 +1,151 @@
 <?php get_header(); ?>
-<div class="main-section activities-section" aria-labelledby="main-section__title activities-section__title">
-	<?php if( have_posts() ): while( have_posts() ): the_post(); $the_artist_id = get_field('artistes')['0']->ID; ?>
-        <article class="main-section__post activities-section__post" aria-labelledby="main-section__post__title activities-section__post__title">
-            <h1 class="main-section__post__title activities-section__post__title" id="main-section__post__title activities-section__post__title" role="heading" aria-level="1">
+<div class="main-section single-activity-container" aria-labelledby="main-section__title single-activity-container__title">
+	<?php if( have_posts() ): while( have_posts() ): the_post(); ?>
+        <article class="main-section__post single-activity-container__post" aria-labelledby="main-section__post__title single-activity-container__post__title">
+            <h1 class="main-section__post__title single-activity-container__post__title" id="main-section__post__title single-activity-container__post__title" role="heading" aria-level="1">
                 <?php the_title(); ?>
             </h1>
             <?php $img_320 =  get_field('thumbnail'); ?>
             <img
-                    class="main-section__post__thumbnail activities-section__post__thumbnail"
+                    class="main-section__post__thumbnail single-activity-container__post__thumbnail"
                     src="<?php echo $img_320['sizes']['320_prev']; ?>"
                     width="<?php echo $img_320['sizes']['320_prev-width']; ?>"
                     height="<?php echo $img_320['sizes']['320_prev-height']; ?>"
                     alt="<?php echo $img_320['alt']; ?>"
             >
-            <ul class="main-section__post__info-list activities-section__post__info-list">
+            <ul class="main-section__post__info-list single-activity-container__post__info-list">
                 <?php foreach(get_field('quand') as $item): ?>
-                    <li class="main-section__post__info-list__info activities-section__post__info-list__info  activities-section__post__info-list__date">
-                        <time class="activities-section__post__info-list__date__day" datetime="<?php echo $item['date']; ?>"><?php ec_the_human_date_from_html_date( $item['date']); ?></time><span class="activities-section__post__info-list__date__time"> de <time class="activities-section__post__info-list__date__time__from" datetime="<?php echo $item['heure_de_debut']; ?>"><?php echo $item['heure_de_debut']; ?></time> à <time class="activities-section__post__info-list__date__time__to" datetime="<?php echo $item['heure_de_fin']; ?>"><?php echo $item['heure_de_fin']; ?></time></span>
+                    <li class="main-section__post__info-list__info single-activity-container__post__info-list__info  single-activity-container__post__info-list__date">
+                        <time class="main-section__post__info-list__date__day__post__info-list__date__day single-activity-container__post__info-list__date__day" datetime="<?php echo $item['date']; ?>"><?php ec_the_human_date_from_html_date( $item['date']); ?></time><span class="main-section__post__info-list__date__day__post__info-list__date__time single-activity-container__post__info-list__date__time"> de <time class="main-section__post__info-list__date__day__post__info-list__date__time__from single-activity-container__post__info-list__date__time__from" datetime="<?php echo $item['heure_de_debut']; ?>"><?php echo $item['heure_de_debut']; ?></time> à <time class="main-section__post__info-list__date__day__post__info-list__date__time__to single-activity-container__post__info-list__date__time__to" datetime="<?php echo $item['heure_de_fin']; ?>"><?php echo $item['heure_de_fin']; ?></time></span>
                     </li>
                 <?php endforeach; ?>
-                <li class="main-section__post__info-list__info activities-section__post__info-list__info activities-section__post__info-list__place">
-                    <span><?= $term_list = wp_get_post_terms($the_artist_id, 'places')[0]->name; ?></span>
-                </li>
-                <?php $term_list = wp_get_post_terms($the_artist_id, 'cat'); if ( count( $term_list ) > 0 ): ?>
-                    <li class="main-section__post__info-list__info main-section__post__info-list__terms activities-section__post__info-list__info activities-section__post__info-list__terms"><!--
-                    <?php if ( count( $term_list ) > 1 ): ?>
-                        <?php for( $i = 0; $i < count( $term_list ); $i++ ): ?>
-                            --><span class="main-section__post__info-list__info__term main-section__post__info-list__terms__term activities-section__post__info-list__info__term activities-section__post__info-list__terms__term"><?php echo ($i == 0 ? $term_list[$i]->name : ', '.$term_list[$i]->name); ?></span><!--
+	            <?php
+	            $artists_list = get_field('artistes');
+	            $terms_list = [];
+	            $places_list = [];
+	            foreach ($artists_list as $an_artist){
+		            foreach (wp_get_post_terms($an_artist->ID, 'cat') as $a_term){
+			            !in_array( $a_term->name, $terms_list) && $terms_list[] =  $a_term->name;
+		            }
+		            foreach (wp_get_post_terms($an_artist->ID, 'places') as $a_place){
+			            !in_array( $a_place->name, $places_list) && $places_list[] =  $a_place->name;
+		            }
+	            }
+	            ?>
+	            <?php if ( count( $places_list ) > 0 ): ?>
+                    <li class="main-section__permalink__post__info-list__info main-section__permalink__post__info-list__terms single-activity-container__permalink__post__info-list__info single-activity-container__permalink__post__info-list__terms"><!--
+                            <?php if ( count( $places_list ) > 1 ): ?>
+                                <?php for( $i = 0; $i < count( $places_list ); $i++ ): ?>
+                                    --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term single-activity-container__permalink__post__info-list__info__term single-activity-container__permalink__post__info-list__terms__term"><?php echo ($i == 0 ? $places_list[$i] : ', '.$places_list[$i]); ?></span><!--
+                                <?php endfor; ?>
+                            <?php else: ?>
+                                --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term single-activity-container__permalink__post__info-list__info__term single-activity-container__permalink__post__info-list__terms__term"><?php echo $places_list[0]; ?></span><!--
+	                        <?php endif; ?>
+                            --></li>
+	            <?php endif; ?>
+                <?php if ( count( $terms_list ) > 0 ): ?>
+                    <li class="main-section__post__info-list__info main-section__post__info-list__terms single-activity-container__post__info-list__info single-activity-container__post__info-list__terms"><!--
+                    <?php if ( count( $terms_list ) > 1 ): ?>
+                        <?php for( $i = 0; $i < count( $terms_list ); $i++ ): ?>
+                            --><span class="main-section__post__info-list__info__term main-section__post__info-list__terms__term single-activity-container__post__info-list__info__term single-activity-container__post__info-list__terms__term"><?php echo ($i == 0 ? $terms_list[$i] : ', '.$terms_list[$i]); ?></span><!--
                         <?php endfor; ?>
                     <?php else: ?>
-                        --><span class="main-section__post__info-list__info__term main-section__post__info-list__terms__term activities-section__post__info-list__info__term activities-section__post__info-list__terms__term"><?php echo $term_list[0]->name; ?></span><!--
+                        --><span class="main-section__post__info-list__info__term main-section__post__info-list__terms__term single-activity-container__post__info-list__info__term single-activity-container__post__info-list__terms__term"><?php echo $terms_list[0]; ?></span><!--
                     <?php endif; ?>
                     --></li>
                 <?php endif; ?>
             </ul>
-            <div class="main-section__post__presentation activities-section__post__presentation">
+            <div class="main-section__post__presentation single-activity-container__post__presentation">
                 <?= get_field( 'presentation') ?>
             </div>
-            <ul class="main-section__post__artists-list activities-section__post__artists-list">
+            <ul class="main-section__post__artists-list single-activity-container__post__artists-list">
 	            <?php foreach(get_field('artistes') as $item): ?>
-                    <li class="main-section__post__artists-list__artist activities-section__post__artists-list__artist">
+                    <li class="main-section__post__artists-list__artist single-activity-container__post__artists-list__artist">
                         <a href="<?= get_permalink($item->ID) ?>"><?= get_post($item->ID)->post_title ?></a>
                     </li>
 	            <?php endforeach; ?>
             </ul>
-
+	
+	        <?php
+	        $items = new WP_Query();
+	        $items->query([
+		        'post_type' => 'activites',
+		        'showposts' => '6',
+		        'post__not_in' => [$post->ID],
+		        'orderby' => 'rand',
+		        'tax_query' => array(
+			        array(
+				        'taxonomy' => 'cat',
+				        'field'    => 'slug',
+				        'terms'    => $terms_list??'',
+			        ),
+		        ),
+	        ]);
+	        ?>
+	        <?php if( $items->have_posts() ): ?>
+                <section class="main-section__post__same-cat-section single-activity-container__post__same-cat-section" aria-labelledby="main-section__post__same-cat-section__title single-activity-container__post__same-cat-section__title">
+                    <h2 class="main-section__post__same-cat-section__title single-activity-container__post__same-cat-section__title" id="main-section__post__same-cat-section__title single-activity-container__post__same-cat-section__title" role="heading" aria-level="2">
+                        Dans la même catégorie
+                    </h2>
+                    <?php while( $items->have_posts() ): $items->the_post(); ?>
+                        <a class="main-section__permalink activities-section__permalink" href="<?php the_permalink(); ?>">
+                            <article class="main-section__permalink__post artists-section__permalink__post" aria-labelledby="main-section__permalink__post__title artists-section__permalink__post__title">
+                                <h3 class="main-section__permalink__post__title artists-section__permalink__post__title" id="main-section__permalink__post__title artists-section__permalink__post__title" role="heading" aria-level="3">
+                                    <?php the_title(); ?>
+                                </h3>
+                                <?php $img_320 =  get_field('thumbnail'); ?>
+                                <img
+                                        class="main-section__permalink__post__thumbnail artists-section__permalink__post__thumbnail"
+                                        src="<?php echo $img_320['sizes']['320_prev']; ?>"
+                                        width="<?php echo $img_320['sizes']['320_prev-width']; ?>"
+                                        height="<?php echo $img_320['sizes']['320_prev-height']; ?>"
+                                        alt="<?php echo $img_320['alt']; ?>"
+                                >
+                                <ul class="main-section__permalink__post__info-list artists-section__permalink__post__info-list">
+                                    <?php
+                                    $artists_list = get_field('artistes');
+                                    $terms_list = [];
+                                    $places_list = [];
+                                    foreach ($artists_list as $an_artist){
+                                        foreach (wp_get_post_terms($an_artist->ID, 'cat') as $a_term){
+                                            !in_array( $a_term->name, $terms_list) && $terms_list[] =  $a_term->name;
+                                        }
+                                        foreach (wp_get_post_terms($an_artist->ID, 'places') as $a_place){
+                                            !in_array( $a_place->name, $places_list) && $places_list[] =  $a_place->name;
+                                        }
+                                    }
+                                    ?>
+                                    <?php if ( count( $places_list ) > 0 ): ?>
+                                        <li class="main-section__permalink__post__info-list__info main-section__permalink__post__info-list__terms activities-section__permalink__post__info-list__info activities-section__permalink__post__info-list__terms"><!--
+                                            <?php if ( count( $places_list ) > 1 ): ?>
+                                                <?php for( $i = 0; $i < count( $places_list ); $i++ ): ?>
+                                                    --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term activities-section__permalink__post__info-list__info__term activities-section__permalink__post__info-list__terms__term"><?php echo ($i == 0 ? $places_list[$i] : ', '.$places_list[$i]); ?></span><!--
+                                                <?php endfor; ?>
+                                            <?php else: ?>
+                                                --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term activities-section__permalink__post__info-list__info__term activities-section__permalink__post__info-list__terms__term"><?php echo $places_list[0]; ?></span><!--
+                                            <?php endif; ?>
+                                        --></li>
+                                    <?php endif; ?>
+                            
+                                    <?php if ( count( $terms_list ) > 0 ): ?>
+                                        <li class="main-section__permalink__post__info-list__info main-section__permalink__post__info-list__terms artists-section__permalink__post__info-list__info artists-section__permalink__post__info-list__terms"><!--
+                                            <?php if ( count( $terms_list ) > 1 ): ?>
+                                                <?php for( $i = 0; $i < count( $terms_list ); $i++ ): ?>
+                                                    --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term artists-section__permalink__post__info-list__info__term artists-section__permalink__post__info-list__terms__term"><?php echo ($i == 0 ? $terms_list[$i] : ', '.$terms_list[$i]); ?></span><!--
+                                                <?php endfor; ?>
+                                            <?php else: ?>
+                                                --><span class="main-section__permalink__post__info-list__info__term main-section__permalink__post__info-list__terms__term artists-section__permalink__post__info-list__info__term artists-section__permalink__post__info-list__terms__term"><?php echo $terms_list[0]; ?></span><!--
+                                            <?php endif; ?>
+                                        --></li>
+                                    <?php endif; ?>
+                                </ul>
+                            </article>
+                        </a>
+                    <?php endwhile; ?>
+                    <a class="main-section__all-posts-page-link activities-section__all-posts-page-link" href="<?php echo get_page_link(13); ?>">Toutes les activités</a>
+                </section>
+	        <?php endif; ?>
+         
         </article>
 	<?php endwhile; endif; ?>
 </div>
