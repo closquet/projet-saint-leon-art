7bac1661a2d4aae8e2e029badee7498a1ab42dd2
Eric Closquet <closquet.eric@live.be>
2018-06-28 01:44:08 +0200

    update gulp for es6

--- app/wp-content/themes/mytheme/gulpfile.babel.js
+++ app/wp-content/themes/mytheme/gulpfile.babel.js
@@ -3,8 +3,10 @@
  * /gulpfile.js - Gulp tasks
  */
 
+'use strict';
+
 //--Config--//
-var src = {
+const src = {
     img: "src/images",
     scss: "src/sass",
     js: "src/js"
@@ -17,53 +19,54 @@ var src = {
 };
 
 
-var gulp = require( "gulp" ),
-    image = require( "gulp-image" ),
-    sass = require( "gulp-sass" ),
-    autoprefixer = require( "gulp-autoprefixer" ),
-    csso = require( "gulp-csso" ),
-    babel = require( "gulp-babel" );
+import gulp from 'gulp';
+import image from 'gulp-image';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+import babel from 'gulp-babel';
 
 // --- Task for images
 
-gulp.task( "images", function() {
+gulp.task( "images", () =>
     gulp.src( src.img + "/**" )
         .pipe( image() )
-        .pipe( gulp.dest( "assets/images" ) );
-} );
+        .pipe( gulp.dest( "assets/images" ) )
+);
 
 // --- Task for styles
 
-gulp.task( "css", function() {
+gulp.task( "css", () =>
     gulp.src( src.scss + "/**/*.scss" )
         .pipe( sass().on( "error", sass.logError ) )
         .pipe( autoprefixer() )
         .pipe( csso() )
-        .pipe( gulp.dest( "assets/css" ) );
-} );
+        .pipe( gulp.dest( "assets/css" ) )
+);
+
 
 // --- Task for js
 
-gulp.task( "js", function() {
+gulp.task( "js", () =>
     gulp.src( src.js + "/**/*.js" )
         .pipe( babel() )
-        .pipe( gulp.dest( "assets/js" ) );
-} );
+        .pipe( gulp.dest( "assets/js" ) )
+);
 
 
 // --- Watch tasks
 
-gulp.task( "watch", function() {
+gulp.task( "watch", () => {
     gulp.watch( src.img + "/**", [ "images" ] );
     gulp.watch( src.scss + "/**/*.scss", [ "css" ] );
     gulp.watch( src.js + "/**/*.js", [ "js" ] );
 
     gulp.watch( dest.css + "/**/*.css" );
     gulp.watch( dest.js + "/**/*.js" );
-} );
+});
 
 // --- Aliases
 
-gulp.task( "default", [ "css", "js" ] );
-gulp.task( "image", [ "images" ] );
-gulp.task( "work", [ "default", "watch" ] );
+
+gulp.task( "default", ['css', 'js'] );
+gulp.task( "work", ['default', 'watch'] );
